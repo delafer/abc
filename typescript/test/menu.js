9f523b0717fc9bda288f97cf9eda9009da3e14ba
TavrovsA <weblogic@bk.ru>
2020-02-13 13:13:23 +0100

    added test html

---
+++ typescript/test/menu.js
@@ -0,0 +1,168 @@
+﻿function indextopmenu(){
+document.write('\
+<td align=left width=116 background="use/bg9l.gif"></td>\
+<td width=529 bgcolor=#EBDCBC>\
+<center>\
+<font size=-2 face="Arial" color=#271F17></font>\
+<font size=-2>[</font>\
+<a href="index.htm"><font size=-2 face="Arial" color=#271F17>Главная</font></a>\
+<font size=-2>]&nbsp;[</font>\
+<a href="source.htm"><font size=-2 color=#271F17 face="Arial">Источники</font></a>\
+<font size=-2>]&nbsp;[</font>\
+<a href="biblio.htm"><font size=-2 color=#271F17 face="Arial">Литература</font></a>\
+<font size=-2>]&nbsp;[</font>\
+<a href="links.htm"><font size=-2 color=#271F17 face="Arial">Ссылки</font></a>\
+<font size=-2>]&nbsp;[</font>\
+<a href="news.htm"><font size=-2 color=#271F17 face="Arial">Новости</font></a>\
+<font size=-2>]&nbsp;[</font>\
+<a href="http://www.abc-guitars.com/gstBook" target="_blank">\
+<font size=-2 color=#271F17 face="Arial">Гостевая книга</font></a>\
+<font size=-2>]&nbsp;[</font>\
+<a href="mailto:s-tavro@mail.ru"><font size=-2 color=#271F17 face="Arial">E-Mail</font></a>\
+<font size=-2>]</font>\
+</center>\
+</td>\
+<td align=right width=115 background="use/bg9r.gif"></td>\
+');
+}
+
+function topmenu(){
+document.write('\
+<td align=left width=116 background="use/bg9l.gif"></td>\
+<td width=529 bgcolor=#EBDCBC>\
+<center>\
+<font size=-2 face="Arial" color=#271F17></font>\
+<font size=-2>[</font>\
+<a href="../index.htm"><font size=-2 face="Arial" color=#271F17>Главная</font></a>\
+<font size=-2>]&nbsp;[</font>\
+<a href="../source.htm"><font size=-2 color=#271F17 face="Arial">Источники</font></a>\
+<font size=-2>]&nbsp;[</font>\
+<a href="../biblio.htm"><font size=-2 color=#271F17 face="Arial">Литература</font></a>\
+<font size=-2>]&nbsp;[</font>\
+<a href="../links.htm"><font size=-2 color=#271F17 face="Arial">Ссылки</font></a>\
+<font size=-2>]&nbsp;[</font>\
+<a href="../news.htm"><font size=-2 color=#271F17 face="Arial">Новости</font></a>\
+<font size=-2>]&nbsp;[</font>\
+<a href="http://www.abc-guitars.com/gstBook" target="_blank">\
+<font size=-2 color=#271F17 face="Arial">Гостевая книга</font></a>\
+<font size=-2>]&nbsp;[</font>\
+<a href="mailto:s-tavro@mail.ru"><font size=-2 color=#271F17 face="Arial">E-Mail</font></a>\
+<font size=-2>]</font>\
+</center>\
+</td>\
+<td align=right width=115 background="use/bg9r.gif"></td>\
+');
+}
+
+
+function indexbottommenu(){
+document.write('\
+<table border=1 width=95% bgcolor=#575757>\
+<tr>\
+<td align=center width=100% bgcolor=#686868>\
+<p class=lk><font size=2 color=#F7E7AF face="Arial">\
+[<a href=index.htm><font color=#F7E7AF>Главная</font></a>]&nbsp;\
+[<a href=about.htm><font color=#F7E7AF>О проекте</font></a>]&nbsp;\
+[<a href=source.htm><font color=#F7E7AF>Источники</font></a>]&nbsp;\
+[<a href=biblio.htm><font color=#F7E7AF>Литература</font></a>]&nbsp;\
+[<a href=links.htm><font color=#F7E7AF>Ссылки</font></a>]&nbsp;\
+[<a href=news.htm><font color=#F7E7AF>Новости</font></a>]&nbsp;\
+[<a href="http://www.abc-guitars.com/gstBook" target="_blank"><font color=#F7E7AF>Гостевая книга</font></a>]&nbsp;\
+[<a href=search.htm target="_blank"><font color=#F7E7AF>Поиск</font></a>]&nbsp;\
+[<a href="mailto:s-tavro@mail.ru"><font color="#F7E7AF">E-mail</font></a>]&nbsp;\
+[<a href=karta.htm><font color=#F7E7AF>Аудио-карта</font></a>]\
+</font></p>\
+</td>\
+</tr>\
+</table>\
+<?echo require_once("se/smart.php"); ?>\
+');
+}
+
+function indexbottommenu1(){
+document.write('\
+<table border=1 width=95% bgcolor=#575757>\
+<tr>\
+<td align=center width=100% bgcolor=#686868>\
+<p class=lk><font size=2 color=#F7E7AF face="Arial">\
+[<a href=about.htm><font color=#F7E7AF>О проекте</font></a>]&nbsp;\
+[<a href=source.htm><font color=#F7E7AF>Источники</font></a>]&nbsp;\
+[<a href=biblio.htm><font color=#F7E7AF>Литература</font></a>]&nbsp;\
+[<a href=links.htm><font color=#F7E7AF>Ссылки</font></a>]&nbsp;\
+[<a href=news.htm><font color=#F7E7AF>Новости</font></a>]&nbsp;\
+[<a href="http://www.abc-guitars.com/gstBook" target="_blank"><font color=#F7E7AF>Гостевая книга</font></a>]&nbsp;\
+[<a href=search.htm target="_blank"><font color=#F7E7AF>Поиск</font></a>]&nbsp;\
+[<a href="mailto:s-tavro@mail.ru"><font color="#F7E7AF">E-mail</font></a>]&nbsp;\
+[<a href=karta.htm><font color=#F7E7AF>Аудио-карта</font></a>]\
+</font></p>\
+</td>\
+</tr>\
+</table>\
+<?echo require_once("se/smart.php"); ?>\
+');
+}
+
+
+function bottommenu(){
+document.write('\
+<table border=1 width="90%" bgcolor="#575757">\
+<tr>\
+<td width="100%" bgcolor="#686868" align=center>\
+<font face="Arial" size=-1 color="#C7B79F">\
+[<a href="../index.htm">Главная</a>]&nbsp;\
+[<a href="../about.htm">О проекте</a>]&nbsp;\
+[<a href="../source.htm">Источники</a>]&nbsp;\
+[<a href="../biblio.htm">Литература</a>]&nbsp;\
+[<a href="../links.htm">Ссылки</a>]&nbsp;\
+[<a href="../news.htm">Новости</a>]&nbsp;\
+[<a href="http://www.abc-guitars.com/gstBook" target="_blank">Гостевая книга</a>]&nbsp;\
+[<a href="mailto:s-tavro@mail.ru">E-mail</a>]&nbsp;\
+[<a href="../search.htm" target="_blank">Поиск</a>]\
+</font>\
+</td>\
+</tr>\
+</table>\
+<?echo require_once("se/smart.php"); ?>\
+');
+}
+
+
+
+
+
+
+
+
+function bottomtext(){
+document.write('\
+<p style="margin-top: 0px; margin-bottom: 6px">\
+<font face="Tahoma" color=#575757 size=2>\
+<p style="margin-top: 3px; margin-bottom: 6px;display: grid;">При использовании материалов<br>ссылка на проект &#171;Гитаристы и Композиторы&#187; ОБЯЗАТЕЛЬНА<br>Все права защищены &#169; Тавровские В.В. и С.В., 2001-2020<br>\
+</font>\
+');
+}
+
+function maincounter(){
+document.write('\
+<td align=center WIDTH="115" background="pages/use/bg9r.gif" valign="bottom">\
+<div align=center style="margin-bottom: 5"><!--TopList-->\
+<a target=_top href="http://top.list.ru/jump?from=235339">\
+<img src="http://top.list.ru/counter?id=235339;t=55" border=0 alt="TopList">\
+</a><!--TopList--><!--Top100--><a href="http://top100.rambler.ru/top100/">\
+<img src="http://top100-images.rambler.ru/top100/banner-88x31-rambler-gray2.gif" alt="Rambler Top100" width=88 height=31 border=0 align="center">\
+</a><!--Top100--></div></td>\
+');
+}
+
+
+function othercounter(){
+document.write('\
+<div align=center style="margin-top: 6; margin-bottom: 6"><!--TopList COUNTER-->\
+<a target=_top href="http://top.list.ru/jump?from=235339">\
+<img src="http://top.list.ru/counter?id=235339;t=55" border=0 height=31 width=88 alt="TopList">\
+</a><!--TopList COUNTER--></div>\
+');
+}
+
+
+
